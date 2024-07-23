e9ce2e36519953d38cf3512f05ec9f2d97ab4635
EkaterinaFefelova <fefelova.ekaterina@gmail.com>
2024-07-23 19:20:16 +0300

    Firing IT specialists - task completed

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,23 @@
+
+
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        for (int i=1; i<=100; i++) {
+            if(i%2!=0)
+                continue;
+            System.out.println("Уволен айтишник с id " + i);
+        }
+        // Решение с использованием только цикла while
+
+        int j = 1;
+        while (j<=100){
+            if(j%2!=0) {
+                j++;
+                continue;
+            }
+            System.out.println("Уволен айтишник с id " + j);
+            j++;
+        }
     }
 }
